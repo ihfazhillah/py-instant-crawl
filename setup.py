114012d3f4c8aab80f480faff66e9e9eda508041
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2019-04-05 22:37:46 +0700

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as readme:
 
 setup(
     name='pyinstantcrawl',
-    version='1.0.0',
+    version='1.0.1',
     description='scrape a website with json template',
     long_description=long_desc,
     url='https://github.com/ihfazhillah/py-instant-crawl',
